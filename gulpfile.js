7319f2e2de10b34dc2a287805029256e5a53516d
Ray Villalobos <ray@planetoftheweb.com>
2018-08-03 19:02:17 -0400

    added generic readme, license notice, target

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var gulp = require('gulp'),
+  webserver = require('gulp-server-io'),
+  source = './builds',
+  dest = './builds';
+
+function html() {
+  return gulp.src(dest + '**/*.html');
+}
+
+function js() {
+  return gulp.src(dest + '**/*.js');
+}
+
+function styles() {
+  return gulp.src(dest + '**/*.css');
+}
+
+function watch() {
+  gulp.watch(source + 'js/**/*.js', js);
+  gulp.watch(source + 'css/**/*.css', styles);
+  gulp.watch(source + 'index.html', html);
+}
+
+function server() {
+  return gulp.src(source).pipe(
+    webserver({
+      serverReload: {
+        dir: source
+      },
+      port: 3333,
+      open: true
+    })
+  );
+}
+
+var build = gulp.series(
+  gulp.parallel(js, styles, html),
+  server,
+  watch
+);
+
+gulp.task('default', build);
