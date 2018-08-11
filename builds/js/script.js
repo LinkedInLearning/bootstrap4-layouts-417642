35334e8ad0cd3cf357fb5f1b341109a9a1b31755
Ray Villalobos <ray@planetoftheweb.com>
2018-08-10 21:09:36 -0400

    starting state

--- builds/js/script.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-  $('#site-modal').on('show.bs.modal', function(event) {
-    $(this)
-      .find('.modal-content img')
-      .attr('src', $(event.relatedTarget).data('highres'));
-  });
-}); // Make sure Document loaded
