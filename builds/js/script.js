9adc4f58f535d751c36e51c20de87d70a30673ef
Ray Villalobos <ray@planetoftheweb.com>
2018-08-11 15:08:32 -0700

    finished video

--- builds/js/script.js
+++ builds/js/script.js
@@ -1,4 +1,16 @@
 $(function() {
+  $(window).on('activate.bs.scrollspy', function() {
+    var hash = $('.site-nav')
+      .find('a.active')
+      .attr('href');
+
+    if (hash !== '#page-hero') {
+      $('header nav').addClass('inbody');
+    } else {
+      $('header nav').removeClass('inbody');
+    }
+  });
+
   $('#site-modal').on('show.bs.modal', function(event) {
     $(this)
       .find('.modal-content img')
