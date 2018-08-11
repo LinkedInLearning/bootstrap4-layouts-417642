f7711dd2338919a179d27000ef45640b8267df94
Ray Villalobos <ray@planetoftheweb.com>
2018-08-11 16:26:08 -0700

    finished video

--- builds/js/script.js
+++ builds/js/script.js
@@ -1,4 +1,31 @@
 $(function() {
+  var topoffset = 55;
+
+  //Use smooth scrolling when clicking on navigation
+  $('.navbar a[href*=\\#]:not([href=\\#])').click(
+    function() {
+      if (
+        location.pathname.replace(/^\//, '') ===
+          this.pathname.replace(/^\//, '') &&
+        location.hostname === this.hostname
+      ) {
+        var target = $(this.hash);
+        target = target.length
+          ? target
+          : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          $('html,body').animate(
+            {
+              scrollTop: target.offset().top - topoffset + 2
+            },
+            500
+          );
+          return false;
+        } //target.length
+      } //click function
+    }
+  ); //smooth scrolling
+
   $(window).on('activate.bs.scrollspy', function() {
     var hash = $('.site-nav')
       .find('a.active')
